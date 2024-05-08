466e7abfbab8125f3033cb1808df28c6329a3b51
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-05-08 17:20:52 +0200

    added DetailPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ function App() {
   return (
     <>
       <Contentful />
-      {/* <hr />
-      <ContentfulCDN />
-      <h1>
-      Hello World!
-      </h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello Tailwind!
-      </h1> */}
     </>
   );
 }
