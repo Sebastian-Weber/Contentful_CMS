1f8247bc04a52835231dd29fc57c37c16ac75156
Tarek <tael3424@th-wildau.de>
2024-05-08 16:59:49 +0200

    changed apicard visual

--- src/components/ApiCard.jsx
+++ src/components/ApiCard.jsx
@@ -1,20 +1,21 @@
-import { useState, useEffect } from "react";
-
 function ApiCard({ id, title, url, alt, description }) {
   return (
     <div>
       <a
         href="#"
-        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        class="block w-80  p-6 bg-slate-700 border border-gray-200 rounded-lg shadow hover:bg-slate-600 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
       >
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black-900 ">
-          {title}
-        </h5>
-        <img src={url} alt={alt} width="20%" />
-
-        <p class="font-normal text-gray-700 dark:text-gray-400">
-          {description}
-        </p>
+        <div className="flex gap-3">
+          <img className="h-10" src={url} alt={alt} />
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-slate-300 ">
+            {title}
+          </h5>
+        </div>
+        <div className="border-t-2 mt-3">
+          <p class="font-normal text-slate-300 dark:text-gray-400 h-20 overflow-hidden">
+            {description}
+          </p>
+        </div>
       </a>
     </div>
   );
