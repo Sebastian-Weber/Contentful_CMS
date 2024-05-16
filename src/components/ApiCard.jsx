a078256139dccffa9bb4bbc39488b599b32f39ac
Tarek <tael3424@th-wildau.de>
2024-05-16 10:32:27 +0200

    added some styling to card

--- src/components/ApiCard.jsx
+++ src/components/ApiCard.jsx
@@ -37,14 +37,14 @@ function ApiCard({ id, title, url, alt, description, popularity, category }) {
 
   return (
     <div>
-      <div className="block w-80  p-6 bg-slate-700 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+      <div className="block w-80 p-6 bg-slate-700 border border-slate-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
         <div className="flex gap-3">
           <img className="h-10" src={url} alt={alt} />
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-300 ">
             {title}
           </h5>
         </div>
-        <div className="border-t-2 mt-3">
+        <div className="border-t-2 h-32 mt-3">
           <p className="font-normal text-slate-300 dark:text-gray-400 h-20 overflow-hidden">
             {description}
           </p>
