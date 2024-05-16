6fa58fd3459fe5810d59f0b07a2f42dfca77ee09
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-05-16 13:29:42 +0200

    detail

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -1,37 +1,55 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import useFetchData from "./FetchData";
+import ApiCard from "./ApiCard";
 import { useParams } from "react-router-dom";
 import CodeMirror from "@uiw/react-codemirror";
-// import axios from "axios";
+
 // import { javascript } from "@codemirror/lang-javascript";
 // import { darcula, darculaInit } from "@uiw/codemirror-theme-darcula";
 // import { darcula } from "@uiw/codemirror-theme-darcula";
 
 function DetailPage() {
-  const [api, setApi] = useState(null);
   const { id } = useParams();
   console.log(id);
+  const { entries } = useFetchData();
+  const apiDetails = {};
+  // const [value, setValue] = useState("console.log('hello world!');");
+  {
+    entries.map((entry) => {
+      if (entry.fields.id === Number(id)) {
+        apiDetails.title = entry.fields.name;
+        apiDetails.description = entry.fields.description;
+        apiDetails.url = entry.fields.icon.fields.file.url;
+        apiDetails.category = entry.fields.category;
+        apiDetails.popularity = entry.fields.popularity;
+        apiDetails.usage = entry.fields.usage;
+      }
+      console.log(entry);
+    });
+  }
 
-  useEffect(() => {
-    // axios
-    //   .get(`htts://fakestoreapi.com/products/${id}`)
-    //   .then((res) => setApi(res.data))
-    //   .catch((err) => console.log(err));
-  }, [id]);
-
-  const [value, setValue] = React.useState("console.log('hello world!');");
-  const onChange = React.useCallback((val, viewUpdate) => {
-    // console.log("val:", val);
-    setValue(val);
-  }, []);
+  // const onChange = React.useCallback((val, viewUpdate) => {
+  //   // console.log("val:", val);
+  //   setValue(val);
+  // }, []);
   return (
     <>
-      <h2>Hallo {id}</h2>
+      <ApiCard
+        width={"50rem"}
+        title={apiDetails.title}
+        url={apiDetails.url}
+        alt={apiDetails.title}
+        description={apiDetails.description}
+        popularity={apiDetails.popularity}
+        category={apiDetails.category}
+      />
+
       <CodeMirror
-        value={value}
+        value={apiDetails.usage}
         height="200px"
         //   extensions={[javascript({ jsx: true })]}
-        onChange={onChange}
+
         //   theme={darcula}
       />
     </>
