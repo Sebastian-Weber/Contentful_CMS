be4ba1fdd016cff283534417cde4ce7668956768
Sebastian Weber <s-weber85@gmx.de>
2024-05-16 17:40:09 +0200

    icons for sidebar added

--- src/components/NavbarLeft.jsx
+++ src/components/NavbarLeft.jsx
@@ -1,141 +1,158 @@
 function NavbarLeft() {
 
-    return (
-      <>
-           
+   return (
+     <>
+          
 <div id="left-menu-box-wrapper" className="justify-center py-3 fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-white w-64 dark:bg-gray-800 transform-none">
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="12" cy="12" r="10" fill="#ffffff"/>
-<path d="M16 14.5005C15.0878 15.7146 13.6356 16.5 12 16.5C10.3642 16.5 8.91221 15.7143 8 14.5M15 9V10M9 9V10M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+
+<div id="icon-container" className="flex flex-row justify-end">
+
+  <svg id="filters" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#5f6368">
+     <path d="M440-120v-240h80v80h320v80H520v80h-80Zm-320-80v-80h240v80H120Zm160-160v-80H120v-80h160v-80h80v240h-80Zm160-80v-80h400v80H440Zm160-160v-240h80v80h160v80H680v80h-80Zm-480-80v-80h400v80H120Z"/>
+  </svg>
+
+  <svg id="arrow-left" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#5f6368">
+     <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/>
+  </svg>
+
+  <svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#5f6368">
+     <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+  </svg>
+
+  <svg id="menu-open" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#5f6368">
+     <path d="M120-240v-80h520v80H120Zm664-40L584-480l200-200 56 56-144 144 144 144-56 56ZM120-440v-80h400v80H120Zm0-200v-80h520v80H120Z"/>
+  </svg>
+
+</div>
+
 <div id="left-bar-menu-box" className="flex flex-col px-4 py-6">
 
-      <div id="left-menu-box-title" className="px-4 mx-2 my-4 font-semibold text-slate-800">Tags</div>
-         <ul className="flex flex-col items-start justify-start">
+     <div id="left-menu-box-title" className="px-4 mx-2 my-4 font-semibold text-slate-800">Tags</div>
+        <ul className="flex flex-col items-start justify-start">
 {/*                
 --- Column --- */}
-               
-         <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Entertainment</a>
-                  </div>
-               </li>
+              
+        <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Entertainment</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Data</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Data</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Music</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Music</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">AI</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">AI</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Education</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Education</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Social</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Social</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Weather</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Weather</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">Finance</a>
-                  </div>
-               </li>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">Finance</a>
+                 </div>
+              </li>
 
-               <li className="my-1 px-2">
-               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                  <a href="" className=" text-sky-900">e-commerce</a>
-                  </div>
-               </li>             
-         </ul>
+              <li className="my-1 px-2">
+              <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                 <a href="" className=" text-sky-900">e-commerce</a>
+                 </div>
+              </li>             
+        </ul>
 
-         {/* --- Wrap --- */}
-         <br></br>
-         <br></br>
+        {/* --- Wrap --- */}
+        <br></br>
+        <br></br>
 
-         <ul className="flex flex-wrap items-start justify-start">
-               
-               <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Entertainment</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Data</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Music</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">AI</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Education</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Social</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Weather</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">Finance</a>
-                        </div>
-                     </li>
-      
-                     <li className="my-1 px-2">
-                     <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
-                        <a href="" className=" text-sky-900">e-commerce</a>
-                        </div>
-                     </li>             
-               </ul>
+        <ul className="flex flex-wrap items-start justify-start">
+              
+              <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Entertainment</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Data</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Music</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">AI</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Education</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Social</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Weather</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">Finance</a>
+                       </div>
+                    </li>
+     
+                    <li className="my-1 px-2">
+                    <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
+                       <a href="" className=" text-sky-900">e-commerce</a>
+                       </div>
+                    </li>             
+              </ul>
 
-      </div>
-    </div>
+     </div>
+   </div>
 
-      </>
-    );
-  }
-  
-  export default NavbarLeft;
+     </>
+   );
+ }
+ 
+ export default NavbarLeft;
