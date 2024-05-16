7dee290eecd5bb83f0e9e98f54fac5a517263fb5
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-05-16 11:52:27 +0200

    detailpage on it's way

--- src/components/ApiCard.jsx
+++ src/components/ApiCard.jsx
@@ -50,23 +50,51 @@ function ApiCard({ id, title, url, alt, description, popularity, category }) {
   }
 
   return (
-    <Link
-      to={`/API-details/${id}`}
-      className="block w-80 h-80 p-6 bg-slate-700 border border-gray-200 rounded-lg shadow hover:bg-slate-600 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-1.5"
-    >
-      <div id={id} className="flex-wrap h-full overflow-hidden">
-        <div className="flex gap-3">
+    <Link to={`/API-details/${id}`}>
+      <div className="flex-wrap">
+        <div className="block w-80 p-6 pb-2 bg-slate-700 border border-slate-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
           <div className="flex row items-end pt-0">
             <img className="h-10 pt-0" src={url} alt={alt} />
             <h6 className="pl-2 text-base leading-5 font-bold tracking-tight text-slate-300 ">
               {title}
             </h6>
           </div>
+
+          <hr className="w-10rem h-0.5 mx-auto md:my-3 bg-gray-100 border-0 rounded dark:bg-gray-700" />
+          <p className="font-normal text-slate-300 dark:text-gray-400 line-clamp-3">
+            {description}
+          </p>
+          <button
+            id="toggle-btn"
+            className="mt-4 text-blue-500 focus:outline-none"
+          >
+            Read More
+          </button>
+
+          <div className="flex gap-20 justify-center mt-2">
+            {popularity ? (
+              <div className="flex gap-4 justify-center">
+                <img src={getEmoji()} alt="" />
+
+                <p className={`font-normal text-gray-300 dark:text-gray-400`}>
+                  {popularity}
+                </p>
+              </div>
+            ) : (
+              <div className="flex gap-4 justify-center">
+                <img className="w-" src={getEmoji()} alt="" />
+
+                <p className="font-normal text-slate-300 dark:text-gray-400">
+                  -
+                </p>
+              </div>
+            )}{" "}
+            {category && <span className={colors[category]}>{category}</span>}
+            {/* <div className="font-normal text-slate-300 dark:text-gray-400">
+            {category}
+          </div> */}
+          </div>
         </div>
-        <hr className="w-10rem h-0.5 mx-auto md:my-3 bg-gray-100 border-0 rounded dark:bg-gray-700" />
-        <p className="font-normal text-slate-300 dark:text-gray-400 h-20 overflow-hidden">
-          {description}
-        </p>
       </div>
     </Link>
   );
