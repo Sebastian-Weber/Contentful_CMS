52b647f7be8f0be9f7f1b7bf083540e924c34ffc
Sebastian Weber <s-weber85@gmx.de>
2024-05-16 10:49:23 +0200

    top navbar with tags design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Contentful from "./components/Contentful";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div>Header</div>
-      <div>
+      <Navbar/>
+      <div className="flex row">
+
         <Contentful itemsPerPage={6} />
       </div>
     </>
