243093ae6dbd8b61c2976c37857bdba4cdee8401
Sebastian Weber <s-weber85@gmx.de>
2024-05-17 10:09:20 +0200

    more styling added

--- src/components/OffCanvas.jsx
+++ src/components/OffCanvas.jsx
@@ -8,9 +8,8 @@ function OffCanvas(){
 
 return (
     <>
-
             {/* icon bar */}
-                <div>
+                {/* <div>
                     <svg id="hamburger-menu" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#ffffff">
                         <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
                     </svg>
@@ -19,23 +18,35 @@ return (
                     <svg id="close-menu" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#ffffff">
                         <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
                     </svg>
-                </div>
+                </div> */}
 
                {/* menu wrapper */}
-                <div id="menu-wrapper" className="justify-center py-3 fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-slate-700 w-64 dark:bg-gray-800 transform-none">
+                <div id="menu-wrapper" className="justify-center fixed h-screen overflow-y-auto transition-transform bg-slate-700 w-64 dark:bg-gray-800 transform-none">
 
-                  <button onClick={()=>setShow(show)} ></button>
                   <div id="iconbar" className="flex flex-row justify-between">
 
-                  <div className=''>
-               {
-              show?<div> 
+                     <div onClick={()=>setShow(!show)}>
+                        <svg id="close-menu" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#ffffff">
+                           <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+                        </svg>
+                   </div>
+               
+               
+            {show?<div>  
+               
 
-                  <div id="Subheading" className="px-4 mx-2 pt-6 font-semibold text-slate-100">Tags</div>
-                  <div id="inner-content-wrapper" className="flex flex-col px-4 py-6">
+               <div id="inner-content-wrapper" className="p-6 bg-slate-600 w-64 dark:bg-gray-800">
+                  <div className='pb-4 flex flex-row justify-start'>
+                  <div className="">
+                     <svg id="close-menu" onClick={()=>setShow(!show)}  xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#ffffff">
+                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
+                     </svg>
+                  </div>
+                  </div>
+                  <div id="Subheading" className="pl-2 pb-2 font-bold text-slate-100">Tags</div>
 
                         {/* --- wrap list items --- */}
-                        <ul className="flex flex-wrap items-start justify-start">
+                        <ul className="flex flex-wrap justify-start">
                               
                            <li className="my-1 px-2">
                               <div className="py-1 px-4 gap-2 font-semibold text-sm rounded-full text-slate-300 dark:text-gray-400 bg-sky-200">
@@ -93,24 +104,12 @@ return (
                                       
                         </ul>
                      </div>
+
               </div>:null  
             }
-                <div onClick={()=>setShow(!show)}>
-                    <svg id="close-menu" xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#ffffff">
-                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-                    </svg>
-                </div>
-        </div>  
-
-                  </div>
-
-                                      
-
-
-
-                </div>
-               
-        </>
+               </div>
+            </div>    
+      </>
 )
 }
 export default OffCanvas;
