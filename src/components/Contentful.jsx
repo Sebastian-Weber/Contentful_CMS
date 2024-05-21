0db7542eac508b3b858264b35eaf94dd09ec0a74
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-05-21 09:39:48 +0200

    shorter

--- src/components/Contentful.jsx
+++ src/components/Contentful.jsx
@@ -173,7 +173,7 @@ function Contentful({ itemsPerPage }) {
             <input
               type="search"
               id="default-search"
-              className="block w-full p-4 ps-10 text-sm w-80 text-gray-900 border border-gray-800 rounded-lg bg-gray-800 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-800 rounded-lg bg-gray-800 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search for API..."
               required
             />
